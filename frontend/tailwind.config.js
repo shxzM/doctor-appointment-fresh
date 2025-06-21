f63308f6d8c83fe8c0fe863d141a536096302452
shxzM <zupeshaz378@gmail.com>
2025-06-21 16:38:34 +0530

    Completed Top Doctors Page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,9 @@ export default {
     extend: {
       colors:{
         'primary': "#5F6FFF"
+      },
+      gridTemplateColumns:{
+        'auto':'repeat(auto-fill, minmax(200px, 1fr))'
       }
     },
   },
