8b63f180c641f41f18db021a882f542322c27bcc
shxzM <zupeshaz378@gmail.com>
2025-06-21 14:25:37 +0530

    Made basic structure

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header'
+import SpecialtyMenu from '../components/SpecialtyMenu'
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <SpecialtyMenu/>
+    </div>
+  )
+}
+
+export default Home
