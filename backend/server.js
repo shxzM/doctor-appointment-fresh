143c8046531473c0b5725f77edf22ca6ee2e9911
shxzM <zupeshaz378@gmail.com>
2025-06-24 13:04:21 +0530

    Connected Cloudinary and MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
 
 
 
@@ -8,6 +10,8 @@ import 'dotenv/config'
 
 const app = express()
 const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
 
 //Middlewares
 
