187cbf01fa24fbe3f9f9590416e281962474eb8e
shxzM <zupeshaz378@gmail.com>
2025-06-25 23:43:20 +0530

    Added Login functionaity for Admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Login from './pages/Login'
 
+import { ToastContainer, toast } from 'react-toastify';
+import { AdminContext } from './context/AdminContext'
+
 const App = () => {
-  return (
-    <div >
-      <Login/>
+
+  const {aToken} = useContext(AdminContext)
+
+  return aToken? (
+    <div>
+      <ToastContainer/>
     </div>
+  ):(
+    <>
+      <Login/>
+      <ToastContainer/>
+    </>
   )
 }
 
