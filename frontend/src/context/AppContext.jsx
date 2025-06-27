fec577f221f94fd387021f4ba79a80f3fbd48041
shxzM <zupeshaz378@gmail.com>
2025-06-27 20:47:37 +0530

    Added the Update details functionality for users

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -11,6 +11,8 @@ const AppContextProvider = (props)=>{
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token')? localStorage.getItem('token'): false)
 
+    const [userData, setUserData] = useState(false)
+
 
     const getDoctorsData = async()=>{
         try {
@@ -27,16 +29,43 @@ const AppContextProvider = (props)=>{
         }
     }
 
+    const loadUserProfileData = async()=>{
+        try {
+            
+            const {data} = await axios.get(backendUrl + '/api/user/get-profile', {headers: {token}})
+            if (data.success) {
+                setUserData(data.userData)
+                
+            }else{
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
     const value ={
         doctors, 
         currencySymbol,
         token, setToken,
-        backendUrl
+        backendUrl, userData, setUserData, 
+        loadUserProfileData
     }
 
     useEffect(()=>{
         getDoctorsData()
     },[])
+
+    useEffect(()=>{
+        if (token) {
+            loadUserProfileData()
+        } else {
+            setUserData(false)
+            
+        }
+    }, [token])
 
     return(
         <AppContext.Provider value={value}>
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,26 +1,62 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const MyProfile = () => {
 
-  const [userData, setUserData] = useState({
-    name: "Edward Vincent",
-    image: assets.profile_pic,
-    email: "edwardvincent@email.com",
-    phone: "+917498795632",
-    address: {
-      line1: "57th Cross Richmond",
-      line2: "Church Road, London"
-    },
-    gender: 'Male',
-    dob: '2002-12-21'
-  })
-
+  const {userData, setUserData, token, backendUrl, loadUserProfileData} = useContext(AppContext)
   const [isEdit, setIsEdit] = useState(false)
+  const [image, setImage] = useState(false)
 
-  return (
+  const updateUserProfileData = async()=>{
+    try {
+      
+      const formData = new FormData()
+
+      formData.append('name', userData.name)
+      formData.append('phone', userData.phone)
+      formData.append('gender', userData.gender)
+      formData.append('dob', userData.dob)
+      formData.append('address', JSON.stringify(userData.address))
+
+      image && formData.append('image', image)
+
+      const {data} = await axios.post(backendUrl + '/api/user/update-profile', formData, {headers: {token}})
+
+      if (data.success) {
+        toast.success(data.message)
+        await loadUserProfileData()
+        setIsEdit(false)
+        setImage(false)
+      } else{
+        toast.error(data.message)
+      }
+
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+
+  return userData && (
     <div className='max-w-lg flex flex-col gap-2 text-sm'>
-      <img className='w-36 rounded' src={userData.image} alt="" />
+      
+      {
+        isEdit
+        ? <label htmlFor="image">
+          <div className='inline-block relative cursor-pointer'>
+            <img className='w-36 rounded opacity-75' src={image ? URL.createObjectURL(image): userData.image} alt="" />
+            <img className='w-10 absolute bottom-12 right-12' src={image ? '': assets.upload_icon} alt="" />
+          </div>
+          <input onChange={(e)=>setImage(e.target.files[0])} type="file" id="image" hidden />
+        </label>
+        : <img className='w-36 rounded' src={userData.image} alt="" />
+      }
+
+      
       {
         isEdit 
         ? <input className='bg-gray-50 text-3xl font-medium max-w-60 mt-4' type="text" value={userData.name} onChange={e=> setUserData(prev=>({...prev, name:e.target.value}))} /> 
@@ -44,10 +80,10 @@ const MyProfile = () => {
           {
             isEdit
             ? <p>
-              <input className='bg-gray-50' onChange={(e)=> setUserData(prev => ({...prev, address, line1 : e.target.value}))} value={userData.address.line1} type="text"/>
-              <br />
-              <input className='bg-gray-50' onChange={(e)=> setUserData(prev => ({...prev, address, line2 : e.target.value}))} value={userData.address.line2} type="text"/>
-            </p>
+                <input className='bg-gray-50' onChange={e => setUserData(prev => ({...prev, address: { ...prev.address, line1: e.target.value }}))} value={userData.address.line1} type="text"/>
+                <br />
+                <input className='bg-gray-50' onChange={e => setUserData(prev => ({...prev,address: { ...prev.address, line2: e.target.value }}))} value={userData.address.line2} type="text"/>
+              </p>
             : <p className='text-gray-500'>
               {userData.address.line1}
               <br />
@@ -78,7 +114,7 @@ const MyProfile = () => {
       <div className='mt-8'>
         {
           isEdit
-          ? <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={()=> setIsEdit(false)}>Save Information</button>
+          ? <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={updateUserProfileData}>Save Information</button>
           : <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all  ' onClick={()=> setIsEdit(true)}>Edit</button>
         }
       </div>
