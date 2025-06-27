e6fd46f98d52734511b2e38748fdf5f9e6071dc6
shxzM <zupeshaz378@gmail.com>
2025-06-27 17:01:04 +0530

    Added login and logout functionality for user

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -9,11 +9,8 @@ const AppContextProvider = (props)=>{
     const currencySymbol = "$"
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [doctors, setDoctors] = useState([])
+    const [token, setToken] = useState(localStorage.getItem('token')? localStorage.getItem('token'): false)
 
-    const value ={
-        doctors, 
-        currencySymbol
-    }
 
     const getDoctorsData = async()=>{
         try {
@@ -28,6 +25,13 @@ const AppContextProvider = (props)=>{
             console.log(error)
             toast.error(error.message)
         }
+    }
+
+    const value ={
+        doctors, 
+        currencySymbol,
+        token, setToken,
+        backendUrl
     }
 
     useEffect(()=>{
